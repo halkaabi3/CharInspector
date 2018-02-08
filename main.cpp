440a4cf7386af830cf0546edccfe429a8fe5e7b0
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-02-08 11:32:15 -0800

    Ascii value and case added.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,30 @@ using namespace std;
 
 int main()
 {
+char x;
+
 
   cout<<"What character do you want to know about?\n";
 
+cin>>x;
+
+
+if(isupper(x)){
   //when user's entry is between A-Z...
   cout<<" is an upper case letter!\n";
-
+}
+  
+else if(islower(x)){
   //when user's entry is between a-z...
   cout<<" is a lower case letter!\n";
+}
 
-  //in all other cases...
+else{
+//in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)x<<endl;
 
   return 0;
 }
